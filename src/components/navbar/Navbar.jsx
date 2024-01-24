1a44421b24a93bf92dce90ea31d447531c019477
xperia7048@gmail.com <xperia7048@gmail.com>
2024-01-24 23:18:23 +0530

    nav bar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import "./navbar.scss"
 import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+
 const Navbar = () => {
   return (
     <div className='navbar'>
@@ -9,7 +16,36 @@ const Navbar = () => {
         <input type="text" placeholder='Search..' />
         <SearchIcon/>
       </div>
-
+      <div className="items">
+        <div className="item">
+          English
+          <LanguageIcon className='icon'/>
+        </div>
+        <div className="item">
+        <DarkModeIcon className='icon'/>
+        </div>
+        <div className="item">
+        <FullscreenIcon className='icon'/>
+        </div>
+        <div className="item">
+        <NotificationsNoneOutlinedIcon className='icon'/>
+        <div className='counter'> 
+          1
+        </div>
+        </div>
+        <div className="item">
+        <ChatOutlinedIcon className='icon'/>
+        <div className='counter'> 
+          2
+        </div>
+        </div>
+        <div className="item">
+        <ListOutlinedIcon className='icon'/>
+        </div>
+        <div className="item">
+        <img src="" alt="" className='avatar'/>
+        </div>
+      </div>
       </div>
     </div>
   )
